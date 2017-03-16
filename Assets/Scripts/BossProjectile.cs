c8d124b64fe93453930570068bab2722b39e15e4
Gurjot <glambsar@uci.edu>
2017-03-16 11:56:06 -0700

    added boss

---
+++ Assets/Scripts/BossProjectile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossProjectile : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if (other.transform.root.gameObject.CompareTag("Player"))
+        {
+            other.transform.root.gameObject.GetComponent<Health>().Hit(50);
+        }
+    }
+}
